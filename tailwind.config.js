3e2742b0485a0954b6fa3433fac2caed7bee3476
HeyFajri <achmatfajri@gmail.com>
2023-04-18 01:24:53 -0800

    fixing all file, folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html, js}'],
+  content: ["./**/*.{html, js}"],
   theme: {
     extend: {
       colors: {
-      'primary' : '#270F59',
-      'secondary' : '#6732D9',
-      'third' : '#854AD9',
-      'fourth' : '#ED85FF',
-      'fifth' : '#4BE2F2',
-      'footer' : '#150930'
-    },
+        primary: "#270F59",
+        secondary: "#6732D9",
+        third: "#854AD9",
+        fourth: "#ED85FF",
+        fifth: "#4BE2F2",
+        footer: "#150930",
+      },
       lineHeight: {
-        '90' : '90px'
+        90: "90px",
       },
       fontSize: {
-        xs: '0.5rem',
-        sm: '0.6rem',
-        base: '0.8rem',
-        lg: '1rem',
-        'xl': '1.300rem',
-        '2xl': '1.400rem',
-        '3xl': '2.500rem',
-        '4xl': '3rem',
-      }},
+        xs: "0.5rem",
+        sm: "0.6rem",
+        base: "0.8rem",
+        lg: "1rem",
+        xl: "1.300rem",
+        "2xl": "1.400rem",
+        "3xl": "2.500rem",
+        "4xl": "3rem",
+      },
+    },
   },
   plugins: [],
-}
-
+};
